82d0ef161b0c9772a485ba6497eced8ce32b51c9
Prajwal Kene <127944958+Prajwakene@users.noreply.github.com>
2023-08-01 04:23:24 +0530

    ThreeJS Ai app

--- Desktop/threeJs/client/src/pages/Home.jsx
+++ Desktop/threeJs/client/src/pages/Home.jsx
@@ -18,13 +18,26 @@ const Home = () => {
       {/* //if we are on homepage */}
       {snap.intro && (
         <motion.section className="home" {...slideAnimation('left')}>
-          <motion.header {...slideAnimation}>
+          <motion.header {...slideAnimation("down")}>
             <img 
             src='./threejs.png'
             alt='logo'
             className='w-8 h-8 object-contain'
             />
           </motion.header>
+          <motion.div className='home-content' {...headContainerAnimation}>
+          <motion.div  {...headTextAnimation}>
+            <h1 className='head-text'>
+              LET's <br className='x1:block hidden' />DO IT.
+            </h1>
+            <motion.div>
+              <p className="max-w-md font-normal text-grey-600 text-base">
+                create your unique and excusive shirt with our brand-new 3D costomization tool.
+                <strong>Unleash your imagination </strong>{" "} and define your own style
+                </p>
+            </motion.div>
+          </motion.div>
+          </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
